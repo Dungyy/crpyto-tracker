66515f6dd125de66fa28e068c7b7e09fba790554
Dungyy <erickmunoz13@gmail.com>
2022-02-16 03:47:50 -0600

    added coins / css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Coin from "./Coin";
+import { tab } from "@testing-library/user-event/dist/tab";
 
 const CoinURL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=500&page=1&sparkline=false";
 
 const App = () => {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
-    axios.get(CoinURL).then((res) => {
-      setCoins(res.data);
-    }).catch(err => console.log(err))
+    axios
+      .get(CoinURL)
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filterCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <h1> API </h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text" style={{ fontSize: "3rem" }}>
+          Dungy Crypto
+        </h1>
+        <form>
+          <input
+            type="text"
+            placeholder="search"
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      <div className=" coin-wrap">
+        
+      {/* insert table */}
+      
+      {filterCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
+
+    </div>
     </div>
   );
 };
