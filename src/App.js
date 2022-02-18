82af43aa156e4c581f1c9a1009b0779af891915a
Dungyy <erickmunoz13@gmail.com>
2022-02-17 22:53:09 -0600

    some quick edits

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import Coin from "./Coin";
-import { tab } from "@testing-library/user-event/dist/tab";
 
 const CoinURL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=500&page=1&sparkline=false";
@@ -32,7 +31,7 @@ const App = () => {
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text" style={{ fontSize: "3rem" }}>
-          Dungy Crypto
+          Dingy
         </h1>
         <form>
           <input
@@ -43,26 +42,23 @@ const App = () => {
           />
         </form>
       </div>
-      <div className=" coin-wrap">
-        
       {/* insert table */}
-      
+
       {filterCoins.map((coin) => {
         return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            image={coin.image}
-            symbol={coin.symbol}
-            marketcap={coin.market_cap}
-            price={coin.current_price}
-            priceChange={coin.price_change_percentage_24h}
-            volume={coin.total_volume}
-          />
+          <div className="coin-wrap">
+            <Coin
+              name={coin.name}
+              image={coin.image}
+              symbol={coin.symbol}
+              marketcap={coin.market_cap}
+              price={coin.current_price}
+              priceChange={coin.price_change_percentage_24h}
+              volume={coin.total_volume}
+            />
+          </div>
         );
       })}
-
-    </div>
     </div>
   );
 };
